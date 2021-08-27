c843ad1bde210832df725974fba53ef3831263ff
lara <MiLara8888@mail.ru>
2021-08-27 13:59:59 +0300

    implementation of transitions between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,14 @@
-from .base_page import BasePage          #импорт базового класса
+from .base_page import BasePage  # импорт базового класса
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocators     #импорт класса с локаторами
+from .locators import MainPageLocators  # импорт класса с локаторами
+from .login_page import LoginPage
 
-class MainPage(BasePage):          #класс главной страницы сайта наследник класса basepage имеет все атрибуты родителя
-    def go_to_login_page(self):       #проверяет кликабельность кнопки зарегестрироваться
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
 
-    def should_be_login_link(self):  #что есть ссылка, которая ведет на логин
+class MainPage(BasePage):  # класс главной страницы сайта наследник класса basepage имеет все атрибуты родителя
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+
+
+    def should_be_login_link(self):  # что есть ссылка, которая ведет на логин
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
