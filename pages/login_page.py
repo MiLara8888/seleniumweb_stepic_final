877294069f38ff8fb9446d88cecd846627d49883
lara <MiLara8888@mail.ru>
2021-08-27 13:05:01 +0300

    registration page file

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import LoginPageLocators        #импорт класса с локаторами
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        url=self.browser.current_url
+        assert 'login' in str(url), f'login not in url {str(url)}'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
