db2f4d260785fb8542c5206204d270573b27767d
lara <MiLara8888@mail.ru>
2021-08-29 12:31:12 +0300

    promo new year

---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+class PageObject(BasePage):
+
+    def correct_addition_to_cart(self):
+        self.add_to_Basket()
+        self.should_match_the_price_of_the_item()
+        self.must_match_the_name_of_the_product()
+
+    def add_to_Basket(self):  # метод добавления в корзину
+        link = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        link.click()
+        self.solve_quiz_and_get_code()
+
+    def should_match_the_price_of_the_item(self):  # цена вкорзине должна соответствоват цене товара
+        message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        assert product_price in message_basket_total, "No product price in the message"
+
+    def must_match_the_name_of_the_product(self):  # продукт в корзине сопадает с выбранным
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
+        assert product_name in message, "No product name in the message"
