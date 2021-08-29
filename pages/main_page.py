1336132fbd14d88a2993954eaf585c2123a3d04f
lara <MiLara8888@mail.ru>
2021-08-29 16:16:07 +0300

    added locators from main

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,10 +4,5 @@ from .login_page import LoginPage
 
 
 class MainPage(BasePage):  # класс главной страницы сайта наследник класса basepage имеет все атрибуты родителя
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-
-
-    def should_be_login_link(self):  # что есть ссылка, которая ведет на логин
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
