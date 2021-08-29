fdd034ab22e6b635c92b33c2973c28070b882c7b
lara <MiLara8888@mail.ru>
2021-08-29 14:10:37 +0300

    parameterization

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,22 @@
 from .pages.main_page import MainPage
 from .pages.product_page import PageObject
+import pytest
+import time
 
-def test_check_registration_forms(browser):
-    link='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019.'
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+                                               "/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    link=link
     page=PageObject(browser,link)
     page.open()
+    time.sleep(2)
     page.correct_addition_to_cart()
