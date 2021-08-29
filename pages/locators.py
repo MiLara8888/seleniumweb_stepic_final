f270dddf1c058f18d020d317e94edde0f00bdaca
lara <MiLara8888@mail.ru>
2021-08-29 12:28:01 +0300

    product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,12 @@ class MainPageLocators():          #каждый класс соответств
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')      #ищем форму логина
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')   #ищем ыорму регистрации
+
+class ProductPageLocators(object):
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
+
