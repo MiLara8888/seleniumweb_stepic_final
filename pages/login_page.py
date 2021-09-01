d7ccfafc1866c59e02303f62900b01eb0bcb9f26
lara <MiLara8888@mail.ru>
2021-09-01 09:47:23 +0300

    register new user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from .locators import LoginPageLocators        #импорт класса с локаторами
+from .locators import LoginPageLocators  # импорт класса с локаторами
+
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,14 +10,19 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        url=self.browser.current_url
+        url = self.browser.current_url
         assert 'login' in str(url), f'login not in url {str(url)}'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
-
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):           #регистрация юзера
+        self.browser.find_element(*LoginPageLocators.USER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.USER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.USER_PASSWORD2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.BUTTON_REGISTRATION).click()
