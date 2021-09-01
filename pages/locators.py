3029677bc2bc00f029ef9df0022d66cd3fe33a1b
lara <MiLara8888@mail.ru>
2021-09-01 09:45:50 +0300

    user icon locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,17 +1,26 @@
 from selenium.webdriver.common.by import By
-#файл для вынесения констант селекторов во внешнююпеременную
+
+
+# файл для вынесения констант селекторов во внешнююпеременную
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    GO_TO_BASKET=(By.CSS_SELECTOR, ".btn-group .btn-default")
+    GO_TO_BASKET = (By.CSS_SELECTOR, ".btn-group .btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
-class MainPageLocators():          #каждый класс соответствует объекту paje object
-    pass     #пара как искать и что искать
+class MainPageLocators():  # каждый класс соответствует объекту paje object
+    pass  # пара как искать и что искать
+
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')      #ищем форму логина
-    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')   #ищем ыорму регистрации
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')  # ищем форму логина
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')  # ищем ыорму регистрации
+    USER_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    USER_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
+    USER_PASSWORD2 = (By.CSS_SELECTOR, '#id_registration-password2')
+    BUTTON_REGISTRATION = (By.CSS_SELECTOR, "[name='registration_submit']")
+
 
 class ProductPageLocators(object):
     BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
@@ -19,10 +28,9 @@ class ProductPageLocators(object):
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
     MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    SUCCESS_MESSAGE= (By.CSS_SELECTOR, ".alert-success")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+
 
 class BasketPageLocators():
-    MESSAGE_BASKET_EMPTY= (By.CSS_SELECTOR,"div#content_inner p")
-    ITEMS_IN_BASKET= (By.CSS_SELECTOR, '.basket-title .row')
-
-
+    MESSAGE_BASKET_EMPTY = (By.CSS_SELECTOR, "div#content_inner p")
+    ITEMS_IN_BASKET = (By.CSS_SELECTOR, '.basket-title .row')
